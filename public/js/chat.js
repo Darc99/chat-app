7ec4370e6b6d77a96d02894ceb942217aee839d8
Darc99 <lennyyhugoh@gmail.com>
2022-01-15 23:52:37 +0100

    broadcasting events and location

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector('#msg-form').addEventListener('submit', (e) => {
 
     socket.emit('sendMsg', message)
 })
+
+document.querySelector('#send-loc').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
