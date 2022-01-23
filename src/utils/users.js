2b8c937ca7bbd9722f71792938d1236273c51e5d
Darc99 <lennyyhugoh@gmail.com>
2022-01-23 23:39:45 +0100

    creating users store

---
+++ src/utils/users.js
@@ -0,0 +1,87 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+//Get User
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+//Get Users in room
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
+
+// addUser({
+//     id: 32,
+//     username: 'Dave  ',
+//     room: '  Lagos'
+// })
+
+// addUser({
+//     id: 12,
+//     username: 'Sam  ',
+//     room: '  Lagos'
+// })
+
+// addUser({
+//     id: 45,
+//     username: 'Simi  ',
+//     room: '  Lagos'
+// })
+
+// console.log(getUser(122))
+// const userList = getUsersInRoom('Lagoos')
+// console.log(userList);
+// console.log(users)
+
+// const removedUser = removeUser(32)
+
+// console.log(removedUser)
+// console.log(users)
